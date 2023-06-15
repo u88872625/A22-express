dbe0a50fc8de52a69203b5786d79bde24fefb59f
u88872625 <u88872625@gmail.com>
2023-06-15 15:07:17 +0800

    Express is running on http://localhost:3000

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(port, () => {
+  console.log(`Express is running on http://localhost:${port}`);
+});
